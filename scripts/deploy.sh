2352e2bbfb4c7a01cce5d4d6eee944aa34c560fd
dydtjd0245 <dydtjd0245@gmail.com>
2020-01-07 22:22:58 +0900

    deploy.sh 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,6 @@
 REPOSITORY=/home/ec2-user/app/step2
 PROJECT_NAME=simple-application
 
-cd $REPOSITORY/$PROJECT_NAME/
-
 echo "> Build 파일 복사"
 
 cp $REPOSITORY/zip/*.jar $REPOSITORY/
@@ -36,6 +34,6 @@ chmod +x $JAR_NAME
 echo "> $JAR_NAME 실행"
 
 nohup java -jar \
-           -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db                      .properties \
+           -Dspring.config.location=classpath:/application.properties,/home/ec2-user/app/application-oauth.properties,/home/ec2-user/app/application-real-db.properties \
            -Dspring.profiles.active=real \
-           $REPOSITORY/$JAR_NAME 2>&1 &
+           $JAR_NAME > $REPOSITORY/nohup.out 2>&1 &
